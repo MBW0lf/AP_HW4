5c4923e74422ba38c928a72e0874b6f14d76dfa4
Mohammad <reyneh.100.mb@gmail.com>
2022-05-03 14:20:38 +0000

    Test1 was passed

--- include/unique_ptr.h
+++ include/unique_ptr.h
@@ -1,14 +1,21 @@
 #ifndef UNIQUE_PTR
 #define UNIQUE_PTR
 
+template<typename T>
 
+class UniquePtr
+{
+public:
+    UniquePtr();
+    UniquePtr(T* _p);
+    UniquePtr(UniquePtr &ptr);
+    ~UniquePtr();
+    T* get();
+    T& operator*();
+    UniquePtr<T>& operator=(const UniquePtr& p);
+private:
+    T* _p;
+};
 
-
-
-
-
-
-
-
-
+#include "unique_ptr.hpp"
 #endif //UNIQUE_PTR
